aaa686ebc04d500a89a91b605c279fa318b26444
P2541037 <P2541037@GWHXX682WIN017W>
2021-02-04 11:25:13 +0000

    Add SQL Staff table, created 1 test class, created 1 libray class that corresponds to the test class.

---
+++ Testing3/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // instance of the class we want to create
+            clsStaff Staff = new clsStaff();
+            //test to see that it exists
+            Assert.IsNotNull(Staff);
+        }
+    }
+}
