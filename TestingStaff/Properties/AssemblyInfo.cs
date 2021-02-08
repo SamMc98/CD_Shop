871f3df1da70da6f43c684077a258ca02a006fab
SamMc98 <73739732+SamMc98@users.noreply.github.com>
2021-02-08 20:07:27 +0000

    Update AssemblyInfo.cs
    Renamed AssemblyTitle and AssemblyProduct.

--- TestingStaff/Properties/AssemblyInfo.cs
+++ TestingStaff/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Testing3")]
+[assembly: AssemblyTitle("TestingStaff")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Testing3")]
+[assembly: AssemblyProduct("TestingStaff")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
