f5422e907ea3d1492797b0d8c48b1df858963ace
SamMc98 <73739732+SamMc98@users.noreply.github.com>
2021-02-19 11:28:42 +0000

    Update AssemblyInfo.cs
    Returned to Testing5 as an error has been made.

--- Testing5/Properties/AssemblyInfo.cs
+++ Testing5/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("TestingOrderLine")]
+[assembly: AssemblyTitle("Testing5")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestingOrderLine")]
+[assembly: AssemblyProduct("Testing5")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
