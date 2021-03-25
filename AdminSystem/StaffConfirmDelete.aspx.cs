9c8b8c26573cb4d3efeaf0422cd84739573dc69f
P2541037 <P2541037@GWHXX682WIN017W>
2021-03-25 14:40:21 +0000

    Finished all week 25 work

--- AdminSystem/StaffConfirmDelete.aspx.cs
+++ AdminSystem/StaffConfirmDelete.aspx.cs
@@ -4,11 +4,50 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_ConfirmDelete : System.Web.UI.Page
 {
+    // var to store the PK value of the record to be deleted
+    Int32 StaffID;
     protected void Page_Load(object sender, EventArgs e)
     {
+        // get the number value of the StaffID to be deleted from the session object
+        StaffID = Convert.ToInt32(Session["StaffID"]);
+    }
 
+    protected void btnYes_Click(object sender, EventArgs e)
+    {
+        // create an instance of the Staff List
+        clsStaffCollection allStaff = new clsStaffCollection();
+
+        // find the record to delete
+        allStaff.ThisStaff.Find(StaffID);
+
+        // delete the reocrd
+        allStaff.Delete();
+
+        // redirect back to the main page
+        Response.Redirect("StaffList.aspx");
+    }
+
+    protected void btnNo_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("StaffList.aspx");
+    }
+
+    protected void btnYes_Click1(object sender, EventArgs e)
+    {
+        // create an instance of the Staff List
+        clsStaffCollection allStaff = new clsStaffCollection();
+
+        // find the record to delete
+        allStaff.ThisStaff.Find(StaffID);
+
+        // delete the reocrd
+        allStaff.Delete();
+
+        // redirect back to the main page
+        Response.Redirect("StaffList.aspx");
     }
 }
--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -49,7 +49,26 @@ public partial class _1_List : System.Web.UI.Page
 
     protected void btnDelete_Click(object sender, EventArgs e)
     {
+        // var to store the PK value of the record to be deleted
+        Int32 StaffID;
 
+        // if a record has been selected from the list
+        if(lstStaffList.SelectedIndex != -1)
+        {
+            // get the PK value of the reocrd to delete
+            StaffID = Convert.ToInt32(lstStaffList.SelectedValue);
+
+            // store the data in the session object
+            Session["StaffID"] = StaffID;
+
+            // redirect to the delete page
+            Response.Redirect("StaffConfirmDelete.aspx");
+        }
+        else // if no record has been selected
+        {
+            //display error
+            lblError.Text = "Please select a record to delete from the list";
+        }
     }
 
     protected void btnEdit_Click(object sender, EventArgs e)
@@ -71,5 +90,43 @@ public partial class _1_List : System.Web.UI.Page
         {
             lblError.Text = "Please select a record from the list";
         }
+    }
+
+    protected void btnApply_Click(object sender, EventArgs e)
+    {
+        // create an instance of the staff collection
+        clsStaffCollection allStaff = new clsStaffCollection();
+
+        allStaff.ReportByName(filterBox.Text);
+
+        lstStaffList.DataSource = allStaff.StaffList;
+
+        // set the name of the PK
+        lstStaffList.DataValueField = "StaffID";
+        // set the name of the field to display
+        lstStaffList.DataTextField = "StaffFullName";
+
+        // bind the data to the list
+        lstStaffList.DataBind();
+    }
+
+    protected void btnClear_Click(object sender, EventArgs e)
+    {
+        // create an instance of the staff collection
+        clsStaffCollection allStaff = new clsStaffCollection();
+        allStaff.ReportByName("");
+        //clear any existing filter to tidy up the interface
+        filterBox.Text = "";
+
+        lstStaffList.DataSource = allStaff.StaffList;
+
+        // set the primary key name
+        lstStaffList.DataValueField = "StaffID";
+
+        // set the name of the field to display
+        lstStaffList.DataTextField = "StaffFullName";
+
+        // bind the data to the list
+        lstStaffList.DataBind();
     }
 }
