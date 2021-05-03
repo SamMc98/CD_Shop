c89c676b6e3937df48a99391fc4c10d6bbc5c872
Ishaaq Manzoor <P2535773@my365.dmu.ac.uk>
2021-05-03 23:08:59 +0100

    Partial completeion of the 'Creating the properties for your table design' sheet to page 3. Commented out the work due to errors to check later.

---
+++ TestingStock/tstStock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using ClassLibary; 
+
+
+namespace TestingStock
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+
+        {
+            //create an insatnce of the class we want to create
+                //clsStock AnStock = new clsStock();
+            //test to see that it exists
+               // Assert.IsNotNull(AnStock);
+
+
+        }
+
+        [TestMethod]
+        public void ActivePropertyOK()
+
+
+        {
+            
+            //create an instance of the class of the we want to create
+               // clsStock = new clsStock();
+            //create some test data to assign to the property 
+               // Boolean TestData = true;
+            //assign the data to the property
+               // AnStock.Active = TestData;
+            //test to see that the two values are the same
+               // Assert.AreEqual(AnStock.Active, TestData);
+
+        }
+    }
+}
